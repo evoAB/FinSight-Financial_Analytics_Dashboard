1773a4a06dc130c321b5e9da917fcb450e61e0f2
Abhishek <abhi@gmail.com>
2025-06-04 01:10:41 +0530

    Updated hosted frontend api

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy =>
     {
         policy.WithOrigins("http://localhost:5173",
-                "fin-sight-frontend.vercel.app")
+                "https://fin-sight-frontend.vercel.app")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
